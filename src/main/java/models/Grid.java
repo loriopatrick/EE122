fbfec4b2452bb9455c653ca52ea33a52d4882390
Patrick Lorio <patrick@lorio.me>
2016-03-08 16:29:12 -0800

    Implement radialQuery for Grid

--- src/main/java/models/Grid.java
+++ src/main/java/models/Grid.java
@@ -1,7 +1,23 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * @author plorio
  */
 public class Grid {
+    private List<Receiver> receivers;
+
+    public Grid() {
+        receivers = new ArrayList<>();
+    }
+
+    public List<Receiver> radialQuery(double x, double y, double start, double end) {
+        return receivers.stream().filter((rcv) -> {
+            double dist = Math.sqrt(Math.pow(x - rcv.x, 2) + Math.pow(y - rcv.y, 2));
+            return dist >= start && dist < end;
+        }).collect(Collectors.toList());
+    }
 }
--- src/main/java/models/Receiver.java
+++ src/main/java/models/Receiver.java
@@ -4,4 +4,11 @@ package models;
  * @author plorio
  */
 public class Receiver {
+    public final float x;
+    public final float y;
+
+    public Receiver(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
 }
